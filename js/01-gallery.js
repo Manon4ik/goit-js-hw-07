f967d0737e137cd402ce3d1383749e7027b315b4
Manon4ik <manon4ik@gmail.com>
2024-05-30 00:59:35 +0300

    02 lightbox - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
-console.log('test');
-
 import { galleryItems } from "./gallery-items.js";
 
-const galleryList = galleryItems.map( ({ preview, original, description }) =>
+const galleryList = galleryItems.map(({ preview, original, description }) =>
   `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img
@@ -23,30 +21,30 @@ gallery.innerHTML = galleryList;
 
 let visiblePopup = false;
 
-document.getElementById('gallery').addEventListener('click', function(event) {
-  event.preventDefault();  
+document.getElementById('gallery').addEventListener('click', function (event) {
+  event.preventDefault();
   if (event.target && event.target.classList.contains('gallery__image')) {
-    
-      const instance = basicLightbox.create(`
+
+    const instance = basicLightbox.create(`
       <img width="1400" height="900" src="${event.target.dataset.source}" alt="${event.target.alt}">
     `,
-    {
-      closable: false,
-      onShow: () => {
-        console.log('is shown');
-      },
-    })
+      {
+        closable: false,
+        onShow: () => {
+          console.log('is shown');
+        },
+      })
     instance.show()
-    visiblePopup = instance.visible()  
+    visiblePopup = instance.visible()
 
 
-    
-// ----- Close popup on Esc ------
-document.addEventListener("keydown", (event)=>{
-  if((event.key == 'Escape' || event.key == 'Esc') && visiblePopup){
-    instance.close();
-  }
-})
+
+    // ----- Close popup on Esc ------
+    document.addEventListener("keydown", (event) => {
+      if ((event.key == 'Escape' || event.key == 'Esc') && visiblePopup) {
+        instance.close();
+      }
+    })
 
   }
 });
