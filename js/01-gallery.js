14fd1f31f50ec07a9b7551def2c5ecd10d8bba53
Manon4ik <manon4ik@gmail.com>
2024-05-29 18:44:19 +0300

    in process

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,15 @@
-import { galleryItems } from "./gallery-items";
+console.log('test');
+
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryList = galleryItems.map(el=>
+  `<div class="gallery__item"><img scr="${el.preview}" /></div>`
+).join("")
+
+const gallery = document.getElementById("gallery")
+
+console.log(gallery);
+
+gallery.innerHTML = galleryList;
